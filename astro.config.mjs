f2b8f17d53f89d7a270a843d9afc5708c3b9c4dc
FrontExtreme01 <frontend.extreme@gmail.com>
2026-06-15 13:57:51 -0500

    FT: Removed dependency astro-seo-schema and update wrangler.jsonc

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@ import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import cloudflare from '@astrojs/cloudflare';
+import node from 'astro/logger/node';
 
 export default defineConfig({
   site: "https://snorkeling.cloudflare.app",
-  output: 'static',
+  output: 'server',
+  adapter: cloudflare(),
   trailingSlash: 'always',
   integrations: [
     react(),
@@ -33,5 +35,4 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  adapter: cloudflare()
 });
